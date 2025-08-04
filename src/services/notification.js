2830e1dfadc7185adfb710f44cc3620bd845dbb8
Amr Badran <badran.amr95@gmail.com>
2025-08-04 11:06:17 +0300

    fix/changed export sendNotification to named export

--- src/services/notification.js
+++ src/services/notification.js
@@ -1,4 +1,4 @@
-const sendNotification = async ({ fcmToken, title, body, data }) => {
+export const sendNotification = async ({ fcmToken, title, body, data }) => {
   const payload = {
     fcmToken: fcmToken,
     title: title,
@@ -15,5 +15,3 @@ const sendNotification = async ({ fcmToken, title, body, data }) => {
     }
   );
 };
-
-export default { sendNotification };
